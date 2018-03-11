1fcd2023b0c0f07148836a89e291582151e40ee0
obwansan <kob123@hotmail.co.uk>
2018-03-11 08:50:09 +0000

    Refactored calcPostsAndRailings()

--- index.php
+++ index.php
@@ -48,17 +48,15 @@
 
                 $length = (float) $_POST['length'];  // convert from string to float
 
-                if ($length < 1.7) {
-                    return '<p>Minimum fence length is 1.7m (1 x 1.5m railing & 2 x 10cm posts)!</p>';
-                }
+                if ($length < 1.7) return '<p>Minimum fence length is 1.7m (1 x 1.5m railing & 2 x 10cm posts)!</p>';
+
                 $length -= 0.1; // subtract start post
-                $postRailUnit = ceil($length / 1.6); // divide by 1 railing / 1 post combo & round up
+                $postRailUnit = ceil($length / 1.6); // divide by 1 railing / 1 post combo & round up to cover overlap
                 $posts = $postRailUnit + 1; // always need 1 more post than railings
                 $railings = $postRailUnit;
 
                 return '<p>Posts: ' . $posts . '</p>' . '<p>Railings: ' . $railings . '</p>' ;
             }
-
             echo calcPostsAndRailings();
 
             ?>
